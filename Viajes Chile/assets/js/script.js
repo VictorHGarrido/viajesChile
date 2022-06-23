ab25eeb4f29965a92bca2c6b6db9c5589b7e9d89
VictorHGarrido <v.garrido619@gmail.com>
2022-06-23 12:38:09 -0400

    Prueba 1

---
+++ Viajes Chile/assets/js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+
+    $('[data-toggle="tooltip"]').tooltip();
+    $("#navbarSupportedContent ul a").on("click", function(e) {
+        e.preventDefault();
+        const href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(href).offset().top }, 2000);
+    });
+
+})
